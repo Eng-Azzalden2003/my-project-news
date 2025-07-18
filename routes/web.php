4d05ae95826f918f22c17e5aee821f0e6efce1db
Azzaldeen Rajab <azzrjb2003@gmail.com>
2025-07-18 14:28:19 +0300

    news

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/index' , function() {
+    return view('news.master');
 });
-Route::get('/home' , function() {
-    return 'home';
-});
+
